b4e5c9525e384b7ee1d20c56dc92e3c27cd4800e
Felipe Gabriel Sampaio Sasaki <fgsasakiacad@gmail.com>
2026-06-17 09:06:51 -0300

    Arquivo tabuleiro.h

--- tabuleiro.c
+++ tabuleiro.c
@@ -3,3 +3,7 @@ Davi Carvalho Lira 605144
 Felipe Gabriel Sasaki 599549
 Roger Levi Forte de Brito 601576
 */
+
+#include <stdio.h>
+#include "tabuleiro.h"
+
--- tabuleiro.h
+++ tabuleiro.h
@@ -3,3 +3,18 @@ Davi Carvalho Lira 605144
 Felipe Gabriel Sasaki 599549
 Roger Levi Forte de Brito 601576
 */
+
+#ifndef TABULEIRO_H
+#define TABULEIRO_H
+
+#define cima_normal 'o';
+#define cima_dama 'O';
+#define baixo_normal '@';
+#define baixo_dama '&';
+#define casa_invalida '#';
+#define casa_vazia ' ';
+
+void inicializar_tabuleiro(char tabuleiro[10][10]);
+void imprimir_tabuleiro(char tabuleiro[10][10]);
+
+#endif
