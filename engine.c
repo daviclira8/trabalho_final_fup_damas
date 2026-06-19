8d2d9663ade44c36cfcb8426c587b6cb5ce9fa92
Roger Levi Forte de Brito <rogerflevi9@gmail.com>
2026-06-18 23:35:04 -0300

    finalzei uma das funcoes.

--- engine.c
+++ engine.c
@@ -19,10 +19,45 @@ int validar_jogada(char tabuleiro[tam_tabuleiro][tam_tabuleiro], char *entrada, 
          && entrada[2] == '-' && entrada[3] == '-' 
          && entrada[4] >= 'A' && entrada[4] <= 'J' 
          && entrada[5] >= '0' && entrada[5] <= '9')) return 0;
+    //convertendo para numero legiveis em uma matriz 10x10;
     int colin = entrada[0] - 'A';
     int linin = entrada[1] - '0'; 
     int colfim = entrada[4] - 'A';
     int linfim = entrada[5] - '0';
+    // checagem da origem e do destino.
+    if(ehposicaovalida(linin, colin) == 0) return 0;
+    if(ehposicaovalida(linfim, colfim) == 0) return 0;
+    //verifica a se a posicao de entrada eh uma casa vazia e se eh a vez do jogador.
+    if(tabuleiro[linin][colin] == casa_vazia) return 0;
+    
+    if(jogadoratual == 'C' && tabuleiro[linin][colin] != cima_normal && tabuleiro[linin][colin] != cima_dama) return 0;
+    if(jogadoratual == 'B' && tabuleiro[linin][colin] != baixo_normal && tabuleiro[linin][colin] != baixo_dama) return 0;
+    
+    if(tabuleiro[linfim][colfim] != casa_vazia) return 0;
+    if(linin == linfim && colin == colfim) return 0;
+
+    if(abs(linfim - linin) != abs(colfim - colin)) return 0;
+    //Verificando se estamos jogando com alguma dama.
+    if(tabuleiro[linin][colin] == cima_dama || tabuleiro[linin][colin] == baixo_dama){
+        int passo_lin = (linfim > linin) ? 1 : -1;
+        int passo_col = (colfim > colin) ? 1 : -1;
+
+        int i = linin + passo_lin;
+        int j = colin + passo_col;
+
+        while(i != linfim){
+            if(tabuleiro[i][j] != casa_vazia) return 0;
+            i = i + passo_lin;
+            j = j + passo_col;
+        }
+    }else{
+        if(abs(linfim - linin) != 1) return 0;
+        if(jogadoratual == 'C' && linfim < linin) return 0; // verificando se foi para frente ou para tras.
+        if(jogadoratual == 'B' && linfim > linin) return 0;
+
+    }
+    
+    
 }
 
 int ehposicaovalida(int lin, int col){
