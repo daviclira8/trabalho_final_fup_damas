6452b6c9550d42420e299c424fa7adf05db540f8
Roger Levi Forte de Brito <rogerflevi9@gmail.com>
2026-06-26 16:48:38 -0300

    Adicionando comentários na minha parte do trabalho

--- engine.c
+++ engine.c
@@ -16,16 +16,16 @@ int pecas_baixo = 15;
 
 //Essa função será responsável por validar as jogadas, verificando a sintaxe e o formato da entrada, validando as coordenadas, as peças e as regras de movimento. 
 //Declarar essas variaveis será util para transformar as entradas em algo legivel dentro de uma matriz 10x10.
-
+// Essa função retorna 0 ou 1, a cada verificação, ela pode retornar 0, encerrando toda a checagem e imprimindo que a jogada não foi válida. 
 int validar_jogada(char *entrada, char jogadoratual){
-    if(strlen(entrada) != 6) return 0;
+    if(strlen(entrada) != 6) return 0; // Verificação do tamanho da entrada, deve ter 6 caracteres.
     if(!(entrada[0] >= 'A' && entrada[0] <= 'J'
          && entrada[1] >= '0' && entrada[1] <= '9' 
          && entrada[2] == '-' && entrada[3] == '-' 
          && entrada[4] >= 'A' && entrada[4] <= 'J' 
-         && entrada[5] >= '0' && entrada[5] <= '9')) return 0;
-    //convertendo para numero legiveis em uma matriz 10x10;
-    int colin = entrada[0] - 'A';
+         && entrada[5] >= '0' && entrada[5] <= '9')) return 0; // Verrificação da sintaxe da entrada, Coordenada de origem a coordenada de destino.  
+    //convertendo para numero legiveis em uma matriz 10x10, pois o jogo só acontece em uma matriz 10x10, o resto dos caracteres são apenas visuais do tabuleiro.
+    int colin = entrada[0] - 'A';// Transformando os caracteres da string em números inteiros através da tabela ASCII.
     int linin = entrada[1] - '0'; 
     int colfim = entrada[4] - 'A';
     int linfim = entrada[5] - '0';
@@ -34,17 +34,17 @@ int validar_jogada(char *entrada, char jogadoratual){
     if(ehposicaovalida(linfim, colfim) == 0) return 0;
     //verifica a se a posicao de entrada eh uma casa vazia e se eh a vez do jogador.
     if(tabuleiro[linin][colin] == casa_vazia) return 0;
-    
+    // Checa qual o jogador da vez. E se a sua casa está correta. 
     if(jogadoratual == 'C' && tabuleiro[linin][colin] != cima_normal && tabuleiro[linin][colin] != cima_dama) return 0;
     if(jogadoratual == 'B' && tabuleiro[linin][colin] != baixo_normal && tabuleiro[linin][colin] != baixo_dama) return 0;
-    
+    // Olha se a casa está vazia e se a linha de entrada é igual a da saída.
     if(tabuleiro[linfim][colfim] != casa_vazia) return 0;
     if(linin == linfim && colin == colfim) return 0;
-
+    // Verifica se a jogada acontece na diagonal.
     if(abs(linfim - linin) != abs(colfim - colin)) return 0;
     //Verificando se estamos jogando com alguma dama.
     if(tabuleiro[linin][colin] == cima_dama || tabuleiro[linin][colin] == baixo_dama){
-        int passo_lin = (linfim > linin) ? 1 : -1;
+        int passo_lin = (linfim > linin) ? 1 : -1; // Verifica se a peça está indo para cima ou para baixo no tabuleiro.
         int passo_col = (colfim > colin) ? 1 : -1;
 
         int i = linin + passo_lin;
@@ -67,7 +67,7 @@ int validar_jogada(char *entrada, char jogadoratual){
     return 1; //Se passou por todas as checagens e não caiu em nenhum return 0, a jogada é válida!
     
 }
-
+// Verifica se a posição é válida.
 int ehposicaovalida(int lin, int col){
     if(lin >= 0 && lin <= 10 - 1 && col >= 0 && col <= 10 - 1){
         if((lin + col) % 2 == 1) return 1;
@@ -83,7 +83,7 @@ void promoverpeca(int lin, int col){
     }
 }
 
-//função feita para processar jogada por jogada de modo a enxutar a int main
+//função feita para processar jogada por jogada de modo a diminuir a int main
 int jogada(char* entrada, char jogador){
     int coluna_inicial = entrada[0] - 'A';
     int linha_inicial = entrada[1] - '0';
